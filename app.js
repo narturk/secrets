40eb113e7281d0a5560ddfb60526f0f8a01e4ae9
nturk <nart_turk@bmc.com>
2023-05-08 21:32:45 +0300

    Using bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -24,10 +27,10 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password']
-});
+// userSchema.plugin(encrypt, {
+//   secret: process.env.SECRET,
+//   encryptedFields: ['password']
+// });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -44,17 +47,20 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
 
-  try {
-    newUser.save();
-    res.render("secrets");
-  } catch (err) {
-    console.log(err);
-  }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    try {
+      newUser.save();
+      res.render("secrets");
+    } catch (err) {
+      console.log(err);
+    }
+  });
 });
 
 app.post("/login", async function(req, res) {
@@ -66,11 +72,13 @@ app.post("/login", async function(req, res) {
       email: username
     });
     if (foundUser) {
-      if (foundUser.password === password) {
-        res.render("secrets");
-      }else{
-      console.log("Password is wrong!");
-      }
+      bcrypt.compare(password, foundUser.password, function(err, result) {
+        if (result == true){
+            res.render("secrets");
+        }else{
+          console.log("Password is wrong!");
+        }
+      });
     } else {
       console.log("User didn`t found!");
     }
